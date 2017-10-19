5faa4f1addca9cbfaaff407bdb595d323b18a330
Vitaly Kononov <vital2207@yandex.ru>
2017-10-20 01:56:06 +0300

    test

--- build/js/all.js
+++ build/js/all.js
@@ -1,9 +1,13 @@
 'use strict';
 
-VK.init(function () {
-  console.log('test');
-  VK.callMethod("showSettingbox", 8214);
-}, function () {
-  throw new Error('Ошибка');
-}, '5.68');
+window.name = 'fXD';
+
+window.onload = function () {
+  VK.init(function () {
+    console.log('test');
+    VK.callMethod("showSettingbox", 8214);
+  }, function () {
+    throw new Error('Ошибка');
+  }, '5.68');
+};
 //# sourceMappingURL=all.js.map
--- source/js/app.js
+++ source/js/app.js
@@ -1,6 +1,13 @@
-VK.init(function() { 
-  console.log('test');
-  VK.callMethod("showSettingbox", 8214);
-}, function() { 
-  throw new Error('Ошибка');
-}, '5.68'); 
+window.name = 'fXD';
+  
+window.onload = ()=>{
+  VK.init(function() { 
+    console.log('test');
+    VK.callMethod("showSettingbox", 8214);
+  }, function() { 
+    throw new Error('Ошибка');
+  }, '5.68'); 
+}
+
+
+
