0f15c320f2c7369d3b176c30147b9432dd3d8b5e
Vitaly Kononov <vital2207@yandex.ru>
2017-10-19 23:23:42 +0300

    commit

--- source/js/app.js
+++ source/js/app.js
@@ -1,1 +1,13 @@
-let a = 1;
+VK.init({
+  apiId: 6226799
+});
+
+let cb = response =>{
+  if(response.session){
+    console.log('Авторизация прошл успешно');
+  } else{
+    console.log('ошибка авторизации')
+  }
+};
+
+VK.auth.login(cb);
