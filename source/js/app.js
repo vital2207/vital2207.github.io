61da28fb148fdaddb776b073ca2e21bce13ccf6b
Vitaly Kononov <vital2207@yandex.ru>
2017-10-20 22:01:35 +0300

    test

---
+++ source/js/app.js
@@ -0,0 +1,13 @@
+window.name = 'fXD';
+  
+window.onload = ()=>{
+  VK.init(function() { 
+    console.log('test');
+    VK.callMethod("showSettingsBox", 8214);
+  }, function() { 
+    throw new Error('Ошибка');
+  }, '5.68'); 
+}
+
+
+
